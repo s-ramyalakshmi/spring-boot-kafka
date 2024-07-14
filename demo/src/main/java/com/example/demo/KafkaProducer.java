85b75b076a9b32e6ec430a3362567cbfc9faf936
Ramyalakshmi <ramy12797@gmail.com>
2024-07-13 17:58:19 -0700

    commit fix

---
+++ demo/src/main/java/com/example/demo/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final String TOPIC = "mytopic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Produced message: " + message);
+    }
+}
